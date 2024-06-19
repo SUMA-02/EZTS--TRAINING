629532b94f995c0980c90b2544d7f176b0aa573c
SUMA-02 <bsumareddy2013@gmail.com>
2024-06-19 11:11:49 +0530

    Add files via upload

---
+++ bubble sort.py
@@ -0,0 +1,8 @@
+#bubble sort
+L=list(map(int,input().split(" ")))
+n=len(L)
+for j in range(0,n):
+    for i in range(0,n-1-j):
+        if L[i]>L[i+1]:
+            L[i],L[i+1]=L[i+1],L[i]  #swap         
+print(L) 
